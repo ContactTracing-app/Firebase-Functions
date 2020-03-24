e752c3d549e35eb574da1a529654fa9f2708010b
Michele Memoli <michele@100shapes.com>
2020-03-24 19:42:25 +0000

    Created profiles

--- functions/src/modules/users.ts
+++ functions/src/modules/users.ts
@@ -2,14 +2,13 @@ import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import { registerUserWithGraph } from './graph';
 
-const db = admin.firestore();
+const fs = admin.firestore();
+const db = admin.database();
 
 export const setupNewUser = functions.auth.user().onCreate(async user => {
   const { uid, displayName, photoURL, email } = user;
 
   const profile = {
-    displayName,
-    photoURL,
     email,
     preferences: {
       contact_via_email: true,
@@ -17,10 +16,14 @@ export const setupNewUser = functions.auth.user().onCreate(async user => {
     }
   };
 
-  await db
-    .collection('profiles')
+  fs.collection('accounts')
     .doc(uid)
     .set(profile);
+
+  db.ref(`profiles/${uid}`).set({
+    displayName,
+    photoURL
+  });
 
   const resp = await registerUserWithGraph({ uid });
 
